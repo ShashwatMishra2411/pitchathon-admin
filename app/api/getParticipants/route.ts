deafcebc66dfd2ce1c479fb2fe3861c965c3ffb4
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-10-01 01:55:24 +0530

    Refactor getParticipants route to handle API errors and update data in page.tsx

--- app/api/getParticipants/route.ts
+++ app/api/getParticipants/route.ts
@@ -3,20 +3,27 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
     try {
-        // Example query to fetch data
-        // console.log(req.type);
-        const result1 = await pool.query('SELECT Count(*) FROM Participants where gender=$1 and present=true', ['Male']);
-        const result2 = await pool.query('SELECT Count(*) FROM Participants where gender=$1 and present=true', ['Female']);
+        // Fetch data from the database
+        const result1 = await pool.query('SELECT Count(*) FROM Participants WHERE gender=$1 AND present=true', ['Male']);
+        const result2 = await pool.query('SELECT Count(*) FROM Participants WHERE gender=$1 AND present=true', ['Female']);
         const result3 = await pool.query('SELECT Count(*) FROM Participants');
-        return new NextResponse(JSON.stringify({
+
+        const response = NextResponse.json({
             male: result1.rows[0].count,
             female: result2.rows[0].count,
             total: result3.rows[0].count
-        }), { status: 200 });
+        }, { status: 200 });
+
+        // Setting headers
+        response.headers.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+        response.headers.set("Pragma", "no-cache");
+        response.headers.set("Expires", "0");
+        response.headers.set("Surrogate-Control", "no-store");
+
+        return response;
+
     } catch (error) {
         console.error('Error fetching participants:', error);
-        return new NextResponse("Failed", { status: 500 });
-        // res.status(500).json({ error: 'Failed to fetch participants' });
+        return NextResponse.json({ error: 'Failed to fetch participants' }, { status: 500 });
     }
 }
-
